ae0891238f9a3876836ab85449522cd8f454e237
Yukimura-Hanzo <t.personal.business@gmail.com>
2025-07-25 15:50:32 +0100

    fix: rm import for budget component

--- src/app/(routes)/dashboard-overview/page.tsx
+++ src/app/(routes)/dashboard-overview/page.tsx
@@ -1,5 +1,5 @@
 //? UI COMPONENTS
-import BudgetCard from "@/app/(ui)/dashboard/financial-log-app/budget-card";
+// import BudgetCard from "@/app/(ui)/dashboard/financial-log-app/budget-card";
 import DateCard from "@/app/(ui)/dashboard/date-card";
 import ProfileCard from "@/app/(ui)/dashboard/profile-card";
 import TodosCard from "@/app/(ui)/dashboard/todo-app/todos-card";
